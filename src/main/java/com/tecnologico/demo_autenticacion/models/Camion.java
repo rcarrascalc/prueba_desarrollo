ea0eb2a369f1cb48f07d6830e5983f81006a64ee
rcarrascalc <r.carrascalc@uniandes.edu.co>
2025-08-21 21:16:41 -0500

    Creacion de modelo de camion

---
+++ src/main/java/com/tecnologico/demo_autenticacion/models/Camion.java
@@ -0,0 +1,13 @@
+package com.tecnologico.demo_autenticacion.models;
+
+public class Camion {
+    private String placa;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
