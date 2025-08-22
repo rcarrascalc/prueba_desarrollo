688d0ae6229535e6ee8b5fb54bb6c489929c2cc9
rcarrascalc <r.carrascalc@uniandes.edu.co>
2025-08-21 21:10:40 -0500

    Commit inicial

---
+++ src/main/java/com/tecnologico/demo_autenticacion/DemoAutenticacionApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologico.demo_autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAutenticacionApplication.class, args);
+	}
+
+}
